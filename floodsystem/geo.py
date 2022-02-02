fd4748f33bf2a818ed9c06417383081619cd45e8
caizicharles <zc348@cam.ac.uk>
2022-02-02 13:58:05 +0000

    added

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -29,6 +29,13 @@ def stations_by_distance(stations, p):
 
 """Task 1C"""
 
+def stations_within_radius(stations, centre, r):
+    distance_from_position = []
+    station_coordinates = []
+    for i in stations:
+        station_coordinates.append(i.coord)
+    for i in station_coordinates
+    for i in range(len(station_coordinates)):
 
 """------------------------------------------"""
 
