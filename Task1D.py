4493d011d02e2a1adc7e99f961e884d4e5bea0a0
caizicharles <zc348@cam.ac.uk>
2022-02-05 18:43:39 +0000

    added

--- Task1D.py
+++ Task1D.py
@@ -5,32 +5,17 @@ from floodsystem.geo import stations_by_river
 def execute_0():  #Number of rivers with at least one station and prints first 10
     
     #Variables
-    stations = build_station_list()                       #Initialize list
-    rivers = rivers_with_station(build_station_list())    #Calls function and stores values
-    river_list0 = []                                      #List of UNSORTED river names
-    river_list = []                                       #Final list of 10 SORTED river names
-    x = 0                                                 #Number count when iterating through list to ch
-    n = 0                                                 #Total number count
-
-    #Check the number of times a river appeared in overall list
-    for river in rivers:
-        for station in stations:
-            if river == station.river:
-                x += 1
-        #Increment n and stores river name
-        if x >= 1:
-            n += 1
-            river_list0.append(river)
-            x = 0
+    rivers = list(rivers_with_station(build_station_list()))    #Calls function and stores values as a list
+    river_list = []                                             #Final list of 10 SORTED river names
 
     #Sort river names in alphabetical order
-    river_list0.sort()
+    rivers.sort()
     
     #Create final list with 10 river names
     for i in range(10):
-        river_list.append(river_list0[i])
+        river_list.append(rivers[i])
         
-    return n, river_list
+    return len(rivers), river_list
 
 #print(execute_0())
 
