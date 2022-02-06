16ea5484a80e05e32711d09beba3c2b591b03a19
caizicharles <zc348@cam.ac.uk>
2022-02-06 21:50:47 +0000

    oh god finally

--- Task1B.py
+++ Task1B.py
@@ -11,20 +11,23 @@ def show():
     show_list_furthest_0 = []                                         #List of tuples of 10 names and furthest distances
     furthest = []                                                     #10 furthest by station, town, distance
 
-    #Create list of tuples with 10 items
+    #Create list of tuples with 10 closest items
     for items in range(0,10):
         show_list_closest_0.append(show_list_0[items])
 
+    #Create list of tuples with 10 furthest items
     for objects in range(len(show_list_0)-10, len(show_list_0)):
         show_list_furthest_0.append(show_list_0[objects])
 
+    #add in closest town names
     for i in show_list_closest_0:
         temp = []
         for x in stations:
             if x.name == i[0]:
                 temp.append((i[0], x.town, i[1]))
                 closest.append(tuple(temp))
-
+    
+    #Add in furthest town names
     for j in show_list_furthest_0:
         temp_0 = []
         for x in stations:
