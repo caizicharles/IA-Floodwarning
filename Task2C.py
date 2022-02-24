2f7480f49f1937ab38ae3e523b15c40934d14411
caizicharles <zc348@cam.ac.uk>
2022-02-24 22:31:05 +0000

    add

--- Task2C.py
+++ Task2C.py
@@ -5,8 +5,10 @@ def run():
 
     stations = build_station_list()
     update_water_levels(stations)
-    station = stations_highest_rel_level(stations, 20)
+    station = stations_highest_rel_level(stations, 7)
+    for n in station:
+        print(n[0], " ", n[1])
 
-    return station
-
-print(run())
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+    run()
