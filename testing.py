9375d5e60227cbec29adc40cd6fbb524c1039513
caizicharles <zc348@cam.ac.uk>
2022-03-01 23:30:03 +0000

    add

--- testing.py
+++ testing.py
@@ -1,12 +1,13 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.analysis import polyfit
+
 import datetime
 import numpy as np
 
 x = build_station_list()
 y = x[0]
-t = 2
-dates, levels = fetch_measure_levels(y.measure_id , dt = datetime.timedelta(days = t))
+dt = 2
+dates, levels = fetch_measure_levels(y.measure_id , dt = datetime.timedelta(days = dt))
 poly, d0 = polyfit(dates, levels, 4)
-print(poly, d0)
+print(poly)
