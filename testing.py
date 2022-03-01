f320bf2e303ccdfaaa142aff97c417cfec8d22ec
caizicharles <zc348@cam.ac.uk>
2022-03-01 09:33:48 +0000

    new

--- testing.py
+++ testing.py
@@ -1,32 +1,37 @@
 from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list, update_water_levels
-
+from floodsystem.utils import sorted_by_key
 from floodsystem.station import inconsistent_typical_range_stations
 
-def testing(stations):
+def testing(stations, tol):
     
     inconsistent_names = inconsistent_typical_range_stations(stations)
-    station_names = set()
-
-    test = set()
+    out = set()
+    out_2 = []
+    out_3 = []
 
     for i in stations:
         for j in inconsistent_names:
             if i.name != j and i.typical_range != None and i.latest_level != None:
-                station_names.add(i.name)
-                test.add(MonitoringStation.relative_water_level(i))
+                out.add((i.name, MonitoringStation.relative_water_level(i)))
+
+    for k in out:
+        out_2.append(k)
+
+    for l in out_2:
+        if l[1] >= tol:
+            out_3.append(l)
+
+    output = sorted_by_key(out_3, 1, reverse=True)
                 
-        
-    return test
+    return output
 
 def run():
     stations = build_station_list()
     update_water_levels(stations)
-    station = testing(stations)
-    #for n in station:
-        #print(n[0], " ", n[1])
-
-    print(station)
+    station = testing(stations, 0.9)
+    for n in station:
+        print(n[0], " ", n[1])
 
 if __name__ == "__main__":
     print("*** Task 2B: CUED Part IA Flood Warning System ***")
