e2cf4e9a5e4366afdfab199edff13993e88499e9
caizicharles <zc348@cam.ac.uk>
2022-03-01 21:43:17 +0000

    new

--- Task2G.py
+++ Task2G.py
@@ -2,15 +2,32 @@ from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.analysis import polyfit
 from floodsystem.datafetcher import fetch_measure_levels
+import numpy as np
 
 def run():
 
     stations = build_station_list()
     update_water_levels(stations)
-    name_ratio = stations_level_over_threshold(stations, 1.3)
-    dates, levels = fetch_measure_levels(i.measure_id , dt = 10)
-    poly, d0 = polyfit(dates, levels, 4)
+    name_ratio = stations_level_over_threshold(stations, 1)
 
+    temp = set()
+
+
+    for i in name_ratio:
+        for j in stations:
+            if i[0] == j.name and j.latest_level != None and j.typical_range != None:
+                temp.add(j)
+
+    for k in temp:
+        dates, levels = fetch_measure_levels(k.measure_id , dt = 10)
+        poly, d0 = polyfit(dates, levels, 4)
+
+        temp = []
+        for x in poly:
+            temp.append(x)
+        
+        d = np.poly1d(temp)
+        d.deriv()
 
 
 
