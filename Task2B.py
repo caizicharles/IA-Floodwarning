4c904a9a8cd8ff1a12fbde619179c92996d578c0
caizicharles <zc348@cam.ac.uk>
2022-03-01 12:17:59 +0000

    new

--- Task2B.py
+++ Task2B.py
@@ -2,12 +2,13 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
 def run():
-    
+
     stations = build_station_list()
     update_water_levels(stations)
     station = stations_level_over_threshold(stations, 0.9)
-    for n in station:
-        print(n[0], " ", n[1])
+    '''for n in station:
+        print(n[0], " ", n[1])'''
+    print(station)
 
 if __name__ == "__main__":
     print("*** Task 2B: CUED Part IA Flood Warning System ***")
--- Task2C.py
+++ Task2C.py
@@ -5,10 +5,9 @@ def run():
 
     stations = build_station_list()
     update_water_levels(stations)
-    station = stations_highest_rel_level(stations, 50)
-    '''for n in station:
-        print(n[0], " ", n[1])'''
-    print(station)
+    station = stations_highest_rel_level(stations, 10)
+    for n in station:
+        print(n[0], " ", n[1])
 
 if __name__ == "__main__":
     print("*** Task 2C: CUED Part IA Flood Warning System ***")
